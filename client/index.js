0271de3d682335955c075d1c34b5a0a1046e8474
YASH PATEL <121890726+yp969803@users.noreply.github.com>
2023-04-12 14:36:02 +0530

    done

--- client/index.js
+++ client/index.js
@@ -1,20 +1,34 @@
-const ipcRenderer=require('electron').ipcRenderer
+const ipcRenderer=window.require('electron').ipcRenderer
+const fs = require("fs")
+console.log('hello world')
 window.onload= function(){
     ipcRenderer.on('uuid',(event,data)=>{
-        document.getElementById('code').innerHTML=data
+        document.getElementById('code').innerText=data
     })
 }
-const startShare=()=>{
+document.getElementById('start').onclick=()=>{
     console.log('start share clicked')
     ipcRenderer.send('start-share',{})
     document.getElementById('start').style.display='none'
     document.getElementById('stop').style.display='block'
 
 }
-function stopShare(){
+document.getElementById('stop').onclick=()=>{
     ipcRenderer.send('stop-share',{})
     document.getElementById('stop').style.display='none'
     document.getElementById('start').style.display='block'
+    document.getElementById('code').innerText=""
 
 }
-
+// const start=document.getElementById('start')
+// console.log('hello world')
+// // start.onclick=(e)=>{
+// //     console.log('helloworld')
+// // }
+// const startShare=()=>{
+//         console.log('start share clicked')
+//         // ipcRenderer.send('start-share',{})
+//         // document.getElementById('start').style.display='none'
+//         // document.getElementById('stop').style.display='block'
+// }
+    
--- client/main.js
+++ client/main.js
@@ -1,8 +1,9 @@
 const { app, BrowserWindow ,ipcMain} = require('electron')
-const{v4,uuidv4}=require('uuid')
+var robot = require("robotjs");
+const{v4:uuidv4}=require('uuid')
 const screenshot=require('screenshot-desktop')
-var socket=require('socket.io-client')('http://103.37.201.175')
-// var interval;
+var socket=require('socket.io-client')('http://localhost')
+var interval;
 
 const path = require('path')
 
@@ -12,13 +13,30 @@ function createWindow () {
     height: 150,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration:true
+      nodeIntegration:true,
+      enableRemoteModule: true,
+      contextIsolation: false,
     }
   })
   win.webContents.openDevTools()
   win.removeMenu()
-
+  
   win.loadFile('index.html')
+  socket.on("mouse-move",(data)=>{
+      var obj=JSON.parse(data);
+      var x=obj.x
+      var y=obj.y
+      robot.moveMouse(x, y)
+
+  })
+  socket.on("mouse-click",(data)=>{
+    robot.mouseClick()
+  })
+  socket.on("mouse-type",(data)=>{
+    var obj=JSON.parse(data)
+    var key=obj.key
+    robot.keyTap(key)
+  })
 }
 
 app.whenReady().then(() => {
@@ -39,13 +57,25 @@ app.on('window-all-closed', () => {
 ipcMain.on('start-share',(event,arg)=>{
     //take contious screen shots
     //broadcast to all other users
-    var uuid=uuidv4;
+    var uuid=uuidv4();
     socket.emit('join-message',uuid)
+    console.log(uuid)
     event.sender.send('uuid',uuid);
-
+    interval=setInterval(()=>{
+       screenshot().then((img)=>{
+        var imgStr=Buffer.from(img).toString('base64')
+        var obj={};
+        obj.room=uuid;
+        obj.image=imgStr;
+        // console.log(imgStr)
+        socket.emit("screen-data",JSON.stringify(obj))
+        console.log('send')
+       })
+    },100)
+    // socket.emit('screen-data','hello world')
 
 })
 ipcMain.on('stop-share',()=>{
     //stop broadcasting & screen capturing
-
+    clearInterval(interval);
 })
